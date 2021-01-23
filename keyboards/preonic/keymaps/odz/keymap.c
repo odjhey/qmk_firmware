92193b8d3a971dae6fde130e563468ba4d0fdc93
odz <odjhey@gmail.com>
2021-01-24 06:39:54 +0800

    preonic/rev3:odz add some special chars mapping

--- keyboards/preonic/keymaps/odz/keymap.c
+++ keyboards/preonic/keymaps/odz/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, RAISE,  LOWER, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LGUI_T(KC_ENT),  KC_SPC,   KC_EQUAL, KC_DEL, LOWER,  RAISE
+  KC_LCTL, LOWER,  RAISE, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LGUI_T(KC_ENT),  KC_SPC,   KC_EQUAL, KC_DEL, LOWER,  RAISE
 ),
 
 /* Colemak
@@ -114,12 +114,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  RGB_TOG, RGB_MOD, _______,  _______, _______,  _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  RGB_TOG, RGB_MOD, _______,  _______, _______,  _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQUAL,
   _______, _______, _______,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   RGB_HUI, RGB_SAI, RGB_VAI,  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_EQUAL, KC_EQUAL, KC_LT, KC_GT,_______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
+
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -136,10 +137,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
-  _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, KC_LCBR, KC_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  _______, KC_LEFT, KC_RGHT, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_DOWN, KC_UP,   KC_LCBR, KC_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, KC_DEL,  KC_INS,  KC_LBRC, KC_RBRC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,
-  _______, _______,  _______,  _______, _______, KC_DEL, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, KC_DEL, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
