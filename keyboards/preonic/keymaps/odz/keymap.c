fafd72dff1dcb54e11a225c2103403211eb8131a
odz <odjhey@gmail.com>
2021-02-08 14:56:20 +0800

    preonic/rev3: update nav mapping

--- keyboards/preonic/keymaps/odz/keymap.c
+++ keyboards/preonic/keymaps/odz/keymap.c
@@ -26,13 +26,16 @@ enum preonic_layers {
   _ADJUST
 };
 
+
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   DVORAK,
   LOWER,
   RAISE,
-  BACKLIT
+  BACKLIT,
+  CBZOOM,
+  CBMUX
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -51,12 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LT(_LOWER, KC_MINS),
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_EQUAL),
-  KC_LCTL, LOWER,  RAISE, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LT(_RAISE, KC_ENT),  KC_SPC,   KC_EQUAL, KC_DEL, LOWER,  LT(_RAISE, KC_EQUAL)
+  KC_LCTL, LOWER,  RAISE, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LT(_RAISE, KC_ENT),  KC_SPC,  KC_RALT, KC_DEL, LOWER, RAISE
 ),
+
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
@@ -115,10 +119,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  RGB_TOG, RGB_MOD, _______,  _______, _______,  _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQUAL,
-  _______, _______, _______,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  RGB_HUI, RGB_SAI, RGB_VAI,  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_EQUAL, _______, KC_LT, KC_GT,_______, _______,
+  RGB_TOG, RGB_MOD, _______,  _______, _______,  _______, _______, KC_AMPR, KC_ASTR, KC_LBRC, CBMUX, _______,
+  _______, KC_HOME, KC_UP,  KC_END, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+  RGB_HUI, KC_LEFT, KC_DOWN,  KC_RIGHT,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, CBZOOM,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_EQUAL, _______, KC_LT, KC_GT,_______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -137,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
+  KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   _______, KC_LEFT, KC_RGHT, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_DOWN, KC_UP,   KC_LCBR, KC_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, KC_DEL,  KC_INS,  KC_LBRC, KC_RBRC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,
@@ -221,6 +225,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             #ifdef __AVR__
             writePinHigh(E6);
             #endif
+          }
+          return false;
+          break;
+        case CBZOOM:
+          if (record->event.pressed) {
+              SEND_STRING(SS_LCTL("b") "z");
+          } else {
+          }
+          return false;
+          break;
+        case CBMUX:
+          if (record->event.pressed) {
+              SEND_STRING(SS_LCTL("b"));
+          } else {
           }
           return false;
           break;
