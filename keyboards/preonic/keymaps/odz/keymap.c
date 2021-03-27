cbb2ae26772a104e9684524d8f3221afed90b72a
odz <odjhey@gmail.com>
2021-03-27 14:45:26 +0800

    feat: add nav to preonic

--- keyboards/preonic/keymaps/odz/keymap.c
+++ keyboards/preonic/keymaps/odz/keymap.c
@@ -23,6 +23,7 @@ enum preonic_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
+  _NAV,
   _ADJUST
 };
 
@@ -33,6 +34,7 @@ enum preonic_keycodes {
   DVORAK,
   LOWER,
   RAISE,
+  NAV,
   BACKLIT,
   CBZOOM,
   CBMUX
@@ -54,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  LT(_RAISE, KC_GRV),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, LT(_LOWER, KC_MINS),
+  LT(_NAV, KC_GRV),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, LT(_LOWER, KC_MINS),
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_EQUAL),
-  KC_LCTL, LOWER,  RAISE, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LT(_RAISE, KC_ENT),  KC_SPC,  KC_RALT, KC_DEL, LOWER, RAISE
+  KC_LCTL, LOWER,  RAISE, KC_LALT, KC_SPC,  LGUI_T(KC_BSPC), LT(_RAISE, KC_ENT),  KC_SPC,  KC_RALT, KC_DEL, KC_RCTL, RAISE
 ),
 
 
@@ -142,10 +144,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, KC_LEFT, KC_RGHT, KC_LPRN, KC_RPRN, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-  _______, KC_DOWN, KC_UP,   KC_LCBR, KC_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  _______, _______, _______, KC_LPRN, KC_RPRN, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+  _______, _______, _______, KC_LCBR, KC_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, KC_DEL,  KC_INS,  KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______
+),
+
+[_NAV] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_LEFT, KC_RGHT, _______, _______,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, 
+  _______, _______, KC_DOWN, KC_UP,   _______, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, 
+  _______, _______, _______, _______, _______, _______,   _______, KC_HOME, KC_END,  _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -208,6 +218,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           } else {
             layer_off(_RAISE);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case NAV:
+          if (record->event.pressed) {
+            layer_on(_NAV);
+          } else {
+            layer_off(_NAV);
           }
           return false;
           break;
