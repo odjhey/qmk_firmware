4c75fde109c5fd47a0bbdb55631f623d5b9aba77
odjhey <odjhey@gmail.com>
2025-12-21 17:33:44 +0800

    idobao: add odeefault

--- keyboards/idobao/id75/keymaps/odeefault/keymap.c
+++ keyboards/idobao/id75/keymaps/odeefault/keymap.c
@@ -17,6 +17,7 @@
 
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define GUI_BSPC MT(MOD_LGUI, KC_BSPC)
+#define L2_MVMT LT(2, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,     KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,     KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,     KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,          GUI_BSPC, KC_ENT,         KC_SPC,  KC_RALT, _______,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,          GUI_BSPC, L2_MVMT,        KC_SPC,  KC_RALT, _______,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
  [1] = LAYOUT_ortho_5x15(
@@ -34,6 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  )
+  ),
 
+  [2] = LAYOUT_ortho_5x15(
+      _______, _______, _______, _______, _______,       _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,       _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,       _______, KC_LEFT,         KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,       _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,       _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______
+  )
 };
