f9aa130b129b675c448287419bce6c2075a969e4
odjhey <odjhey@gmail.com>
2025-12-21 13:32:34 +0800

    idobo: update odeefault

--- keyboards/idobao/id75/keymaps/odeefault/keymap.c
+++ keyboards/idobao/id75/keymaps/odeefault/keymap.c
@@ -15,14 +15,17 @@
  */
 #include QMK_KEYBOARD_H
 
+#define CTL_ESC MT(MOD_LCTL, KC_ESC)
+#define GUI_BSPC MT(MOD_LGUI, KC_BSPC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [0] = LAYOUT_ortho_5x15(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,            KC_5,    KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,    KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,            KC_G,    KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,          KC_BSPC,  KC_ENT,        KC_SPC,  KC_RALT, _______,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,            KC_5,     KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,     KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,     KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,     KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
+    KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,          GUI_BSPC, KC_ENT,         KC_SPC,  KC_RALT, _______,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
  [1] = LAYOUT_ortho_5x15(
