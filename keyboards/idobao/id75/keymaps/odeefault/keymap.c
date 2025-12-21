50ea62cc63e504be5208f5b031700c1e3f0e00cf
odjhey <odjhey@gmail.com>
2025-12-21 17:54:38 +0800

    idobao: update odeefault
    add Makefile.odz for ease of recompiling

--- keyboards/idobao/id75/keymaps/odeefault/keymap.c
+++ keyboards/idobao/id75/keymaps/odeefault/keymap.c
@@ -18,13 +18,14 @@
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define GUI_BSPC MT(MOD_LGUI, KC_BSPC)
 #define L2_MVMT LT(2, KC_ENT)
+#define CTL_ENT MT(MOD_RCTL, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [0] = LAYOUT_ortho_5x15(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,            KC_5,     KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,     KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,     KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,     KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, CTL_ENT,  KC_ENT,  KC_PGUP,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,     KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
     KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,          GUI_BSPC, L2_MVMT,        KC_SPC,  KC_RALT, _______,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
   ),
